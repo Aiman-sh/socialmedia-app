c8703ee46600726de0db822ad17ab2ad2929c4ef
pooja-kaniya <110602998+pooja-kaniya@users.noreply.github.com>
2022-08-11 19:16:56 +0500

    Add files via upload

---
+++ src/dummyData.js
@@ -0,0 +1,143 @@
+export const Users = [
+    {
+        id:1,
+        profilePicture: "assets/person1.jpg",
+        username: "Timmy Drew",
+    },
+    {
+        id:2,
+        profilePicture: "assets/person2.jpg",
+        username: "Janell Shrum",
+    },
+    {
+        id:3,
+        profilePicture: "assets/person3.jpg",
+        username: "Alex Durden",
+    },
+    {
+        id:4,
+        profilePicture: "assets/person4.jpg",
+        username: "Dora Hawks",
+    },
+    {
+        id:5,
+        profilePicture: "assets/person5.jpg",
+        username: "Thomas Holden",
+    },
+    {
+        id:6,
+        profilePicture: "assets/person6.jpg",
+        username: "Shirley Beauchamp",
+    },
+    {
+        id:7,
+        profilePicture: "assets/person7.jpg",
+        username: "Travis Bennett",
+    },
+    {
+        id:8,
+        profilePicture: "assets/person8.jpg",
+        username: "Kristen Thomas",
+    },
+    {
+        id:9,
+        profilePicture: "assets/person9.jpg",
+        username: "Gary Duty",
+    },
+    {
+        id:10,
+        profilePicture: "assets/person10.jpg",
+        username: "Safak Kocaoglu",
+    },
+];
+
+export const Posts =[
+    {
+        id:1,
+        desc: "Love for All, Hatred for None.",
+        photo: "assets/1.jpg",
+        date: "5 mins ago",
+        userId: 1,
+        like: 32,
+        comment: 9,
+    },
+    {
+        id:2,
+        photo: "assets/2.jpg",
+        date: "15 mins ago",
+        userId: 2,
+        like: 2,
+        comment: 1,
+    },
+    {
+        id:3,
+        desc: "Every moment is a fresh begnning .",
+        photo: "assets/3.jpg",
+        date: "12 mins ago",
+        userId: 3,
+        like: 12,
+        comment: 5,
+    },
+    {
+        id:4,
+        photo: "assets/4.jpg",
+        date: "1 hours ago",
+        userId: 4,
+        like: 19,
+        comment: 3,
+    },
+    {
+        id:5,
+        photo: "assets/5.jpg",
+        date: "5 hours ago",
+        userId: 5,
+        like: 23,
+        comment: 8,
+     },
+     {
+        id:6,
+        desc: "Never regret anything that made you smile.",
+        photo: "assets/6.jpg",
+        date: "9 hours ago",
+        userId: 6,
+        like: 39,
+        comment: 14,
+     },
+     {
+        id:7,
+        photo: "assets/7.jpg",
+        date: "5 hours ago",
+        userId: 7,
+        like: 23,
+        comment: 8,
+     },
+     {
+        id:8,
+        photo: "assets/8.jpg",
+        date: "23 hours ago",
+        userId: 8,
+        like: 15,
+        comment: 2,
+     },
+     {
+        id:9,
+        desc: "change the world by being yourself",
+        photo: "assets/9.jpg",
+        date: "3 daysago",
+        userId: 9,
+        like: 44,
+        comment: 15,
+     },
+     {
+        id:10,
+        photo: "assets/10.jpg",
+        date: "1 daysago",
+        userId: 10,
+        like: 14,
+        comment: 1,
+     },
+   
+    
+    
+
+];
